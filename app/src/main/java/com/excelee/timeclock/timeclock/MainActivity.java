7675a9f7d7a7c1642818605ae3eb95d605ce7c40
lynnlee_mac <cnexcelee@gmail.com>
2015-12-17 23:19:26 +0800

    time clock first commit

---
+++ app/src/main/java/com/excelee/timeclock/timeclock/MainActivity.java
@@ -0,0 +1,49 @@
+package com.excelee.timeclock.timeclock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** 添加闹钟按钮 **/
+    Button btn_addClock;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    /**
+     * 初始化view
+     */
+    public void initView(){
+
+        btn_addClock = (Button) findViewById(R.id.mainactivity_btn_addclock);
+
+
+        btn_addClock.setOnClickListener(new MyClickListener());
+
+    }
+
+    /**
+     * 点击事件
+     */
+    class MyClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Log.i("TAG","Click !!!");
+            startActivity(new Intent(MainActivity.this,AddClockActivity.class));
+            finish();
+        }
+    }
+}
